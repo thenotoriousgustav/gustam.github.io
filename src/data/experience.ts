7f96207e9a119b150bf67a4ed79b805e167b8d0a
thenotoriousgustav <rhezagustam@gmail.com>
2024-06-09 17:13:50 +0700

    new experiences

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,34 +1,56 @@
 const EXPERIENCE = [
   {
+    name: "Kementerian Keuangan Republik Indonesia",
+    dates: {
+      start: "02.2024",
+      end: "06.2024",
+    },
+    description: `During my internship at the Ministry of Finance, I significantly contributed to modernizing their web infrastructure. 
+    I upgraded the Next.js framework from version 9 to 14, incorporating the latest features and performance improvements. 
+    I also enhanced code readability and maintainability by converting class components into functional components.
+    Additionally, I developed KEMENKEU CAT, an AI-based exam website designed to ensure exam integrity through advanced technologies such as face recognition,
+    voice activity detection, OCR, and object detection. This project demonstrated my ability to integrate
+    various AI and machine learning technologies into a cohesive and robust system for secure online examinations.
+    
+    `,
+    position: "Full-Stack Developer",
+    link: "https://indonesia.properti.com/",
+  },
+  {
     name: "INDONESIA PROPERTI",
     dates: {
-      start: "09.2023",
-      end: "10.2023",
+      start: "04.2023",
+      end: "07.2023",
     },
-    description:
-      "Building a Real Estate Property platform that deepens connections that matter to you.",
+    description: `Played a crucial role as a Full Stack Developer, 
+    where I focused on developing and enhancing a dynamic property listing website. 
+    My responsibilities included utilizing cutting-edge technologies to deliver a highly interactive, 
+    efficient, and user-friendly platform for property management and browsing.`,
     position: "Full-Stack Developer",
     link: "https://indonesia.properti.com/",
   },
   {
     name: "GSTM STUDIO",
     dates: {
-      start: "09.2021",
-      end: "09.2021",
+      start: "09.2022",
+      end: "03.2023",
     },
-    description:
-      "Build and Develop a website, a company that provides services in the field of UI/UX, Web Development and App Development.",
+    description: `GSTM Studio is a digital studio born from a college project, 
+    our journey began as a simple academic endeavor, but swiftly evolved into a bold vision: to redefine digital experiences
+    through our digital studio website. With diverse expertise and boundless passion,
+    we've built GSTM Studio into a hub for creative collaboration and groundbreaking digital solutions.`,
     position: "Full-Stack Developer",
     link: "https://autolabproject.com/",
   },
   {
     name: "PT. DUTA SAMUDERA MAHENDRA",
     dates: {
-      start: "09.2021",
-      end: "09.2021",
+      start: "12.2021",
+      end: "5.2022",
     },
-    description:
-      "Designing & building new features for an open-source course management system that allows professors to autograde student programming assignments— in any language, using any software package, instantly.",
+    description: `Developed a comprehensive company profile website designed to
+      present the company's brand, services, and values in a visually appealing and highly functional manner and a property listing website aimed at
+      providing users with an efficient and user-friendly platform to explore and manage property listings.`,
     position: "Software Developer (Prev, Team Lead)",
     link: "https://autolabproject.com/",
   },
